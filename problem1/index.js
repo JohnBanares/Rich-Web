295ab6c02fae7b8dada05a40d8f291229cef2ce8
JohnBanares <jmbanares02@gmail.com>
2023-10-17 11:53:40 +0100

    test search

--- problem1/index.js
+++ problem1/index.js
@@ -1,24 +1,37 @@
 const searchBox = document.getElementById('searchBox');
 const table = document.getElementById('resultList');
 const rows = document.getElementsByTagName('tr');
+const warning = document.getElementById("noResult")
 
 searchBox.addEventListener('input', function() {
     const searchText = searchBox.value.toLowerCase();
     filterItems(searchText);
 });
 
+
+
 function filterItems(searchText) {
-    Array.from(rows).forEach(function(row, index) {
-        if (index !== 0) { 
-            const text = row.textContent.toLowerCase();
+    let result = false;
+
+    for (const row of rows) {
+        if (row.rowIndex !== 0) {
+            const td = row.getElementsByTagName("td")[1]; 
+            const text = td.textContent.toLowerCase();
             if (text.includes(searchText)) {
                 row.style.display = 'table-row';
+                result = true;
             } else {
                 row.style.display = 'none';
+                result = false
             }
         }
-    });
+    }
+
+    if(result == false){
+        warning.style.visibility = 'visible';
+    }
+    else{
+        warning.style.visibility = 'hidden';
+    }
 }
-
-
 
