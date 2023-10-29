5a526261f120ba2a3d09995afeb30ed6048cf1f4
JohnBanares <jmbanares02@gmail.com>
2023-10-29 15:18:56 +0000

    readme and part 2

--- problem2/index.js
+++ problem2/index.js
@@ -4,8 +4,35 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         const part1 = data
             .map(data => data.title)
             .filter(title => title.split(' ').length > 6); 
-
-        
-
         console.log(part1);
+
+        //get data body content
+        const getBody = data.map(data => data.body);
+        //this is getting the body content and spliting the array into individual words including spaces ane new lines
+        const trimBody = getBody
+        .map(body => body.split(/\s+/));
+
+        //merge subarrays into one array
+        const flatBody = trimBody.flat();
+
+        //iterate over each element of flatBody and update value of countWord
+        const countBody= flatBody.reduce((countWord, word) => {
+            //empty countWord stores the count of each word
+            if (word in countWord) {
+                countWord[word] += 1;
+            } else {
+                countWord[word] = 1; 
+            }
+            return countWord;
+        },{})
+
+        // console.log(getBody);
+        // console.log(trimBody);
+        // console.log(flatBody);
+        console.log(countBody);
     })
+
+
+
+
+
--- problem2/tempCodeRunnerFile.js
+++ problem2/tempCodeRunnerFile.js
@@ -1,3 +1,26 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(data => {
+//         const part1 = data
+//             .map(data => data.title)
+//             .filter(title => title.split(' ').length > 6); 
+
+        
+
+//         console.log(part1);
+//     })
+// fetch('http://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(posts => {
+//         const getBody = posts.map(post => post.body);
+//         const trimBody = getBody
+//         .map(body => body.trim().split(/\s+/));
+//         const flatBody = trimBody.flat();
+
+    
+//         // console.log(getBody);
+//         // console.log(trimBody);
+//         console.log(flatBody);
+//         //console.log('Word freq;uency map:', wordFrequencyMap);
+//     })
+
