d85df2d41b57d56203ccdf79d9dc8b8f5ff94373
JohnBanares <jmbanares02@gmail.com>
2023-11-20 22:23:18 +0000

    parent

--- problem1/index.js
+++ problem1/index.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('load', ()=> {
     const { fromEvent } = rxjs;
     const form_note = document.getElementById('form_note');
@@ -16,13 +17,82 @@ window.addEventListener('load', ()=> {
             text[i].style.backgroundColor = color;
         }
     }
+    class Note {
+        constructor(parent, text) {
+            this.parent = parent;
+            this.element = this.createNoteElement(text);
+        }
+    
+        createNoteElement(text) {
+            const new_note = document.createElement('div');
+            new_note.classList.add('note');
+    
+            const new_words = document.createElement('div');
+            new_words.classList.add('words');
+            new_note.appendChild(new_words);
+    
+            const new_text = document.createElement('input');
+            new_text.classList.add('text');
+            new_text.type = 'text';
+            new_text.value = text;
+            new_text.setAttribute('readonly', 'readonly');
+    
+            new_words.appendChild(new_text);
+    
+            const new_functions = document.createElement('div');
+            new_functions.classList.add('functions');
+    
+            const new_edit = document.createElement('button');
+            const new_delete = document.createElement('button');
+    
+            new_edit.classList.add('edit');
+            new_delete.classList.add('delete');
+    
+            new_edit.innerText = 'Edit';
+            new_delete.innerText = 'Delete';
+    
+            new_functions.appendChild(new_edit);
+            new_functions.appendChild(new_delete);
+            new_note.appendChild(new_functions);
+    
+            list_notes.appendChild(new_note);
+    
+            // Create observables and subscribe to them
+            fromEvent(new_functions, 'click')
+                .subscribe(() => {
+                    if (new_edit.innerText == 'Edit') {
+                        new_text.removeAttribute('readonly');
+                        new_text.focus();
+                        new_edit.innerText = 'Save';
+                    } else {
+                        new_text.setAttribute('readonly', 'readonly');
+                        new_edit.innerText = 'Edit';
+                    }
+                });
+    
+            // Create observables and subscribe to them
+            fromEvent(new_delete, 'click')
+                .subscribe(() => {
+                    list_notes.removeChild(new_note);
+                    if (this.parent) {
+                        this.parent.removeChild(this);
+                    }
+                });
+    
+            return new_note;
+        }
+    
+        removeChild(child) {
+            this.element.removeChild(child.element);
+        }
+    }
 
     // Create observables and subscribe to them
     fromEvent(red_button, 'click')
         .subscribe(() => switchColor('red'));
 
     fromEvent(blue_button, 'click')
-        .subscribe(() => switchColor('orange'));
+        .subscribe(() => switchColor('blue'));
 
     fromEvent(yellow_button, 'click')
         .subscribe(() => switchColor('yellow'));
@@ -34,62 +104,13 @@ window.addEventListener('load', ()=> {
     //create observable
     const formSubmit$ = fromEvent(form_note, 'submit');
 
-    // subscribe to observable
+    // subscribe to observable, create note class
     formSubmit$.subscribe((e) => {
         e.preventDefault();
 
-        const note = text_box.value;
+        const noteText = text_box.value;
 
-        const new_note = document.createElement('div');
-        new_note.classList.add('note');
-
-        const new_words = document.createElement('div');
-        new_words.classList.add('words');
-        new_note.appendChild(new_words);
-
-        const new_text = document.createElement('input');
-        new_text.classList.add('text');
-        new_text.type = 'text';
-        new_text.value = note;
-        new_text.setAttribute('readonly', 'readonly');
-
-        new_words.appendChild(new_text);
-
-        const new_functions = document.createElement('div');
-        new_functions.classList.add('functions');
-
-        const new_edit = document.createElement('button');
-        const new_delete = document.createElement('button');
-
-        new_edit.classList.add('edit');
-        new_delete.classList.add('delete');
-
-        new_edit.innerText = 'Edit';
-        new_delete.innerText = 'Delete';
-
-        new_functions.appendChild(new_edit);
-        new_functions.appendChild(new_delete);
-        new_note.appendChild(new_functions);
-        list_notes.appendChild(new_note);
-        
-        // Create observables and subscribe to them
-        fromEvent(new_functions, 'click')
-            .subscribe(() => {
-                if (new_edit.innerText == 'Edit') {
-                    new_text.removeAttribute('readonly');
-                    new_text.focus();
-                    new_edit.innerText = 'Save';
-                } else {
-                    new_text.setAttribute('readonly', 'readonly');
-                    new_edit.innerText = 'Edit';
-                }
-            });
-        // Create observables and subscribe to them
-        fromEvent(new_delete, 'click')
-            .subscribe(() => {
-                list_notes.removeChild(new_note);
-            });
+        const newNote = new Note(null, noteText);
     });
-
 
 });
